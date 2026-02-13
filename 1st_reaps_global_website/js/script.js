fc273980a5e901b41c599695fc9594a63b450ff5
Tioluwanimofe Adesanya <86200511+tioluwanimofe@users.noreply.github.com>
2026-02-13 10:34:21 -0600

    Delete 1st_reaps_global_website directory

--- 1st_reaps_global_website/js/script.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Thank you for contacting 1st Reaps Global Company Nig Ltd. We will get back to you shortly.");
-});
