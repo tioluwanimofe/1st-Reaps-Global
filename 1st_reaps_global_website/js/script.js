87850b1e206f5234dbdcc5d8b314a3779ab02441
Tioluwanimofe Adesanya <86200511+tioluwanimofe@users.noreply.github.com>
2026-02-13 08:56:29 -0600

    Add files via upload

---
+++ 1st_reaps_global_website/js/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you for contacting 1st Reaps Global Company Nig Ltd. We will get back to you shortly.");
+});
